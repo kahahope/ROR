a96aa34d74dadb74280fd52264a2ee57daa59096
kahahope <kahahope@gmail.com>
2016-11-16 19:11:29 +0400

    Add files via upload

--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -1,1 +1,13 @@
+puts "Как Вас зовут?"
+name = gets.chomp
 
+puts "Какой у Вас рост?"
+height = gets.to_i
+
+ideal_weight = height - 110
+
+if ideal_weight >= 0
+	"#{name}, Ваш оптимальный вес #{ideal_weight} кг."
+else
+	"#{name}, Ваш вес уже оптимален."
+end
