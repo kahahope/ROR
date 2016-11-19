509f2b1e634dcbeeb54c7ce54effb72b80a1b6c9
kahahope <kahahope@gmail.com>
2016-11-19 04:17:00 +0400

    Update weight.rb
    Set up soft tabs in Sublime.

--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -7,7 +7,7 @@ height = gets.to_i
 ideal_weight = height - 110
 
 if ideal_weight >= 0
-	"#{name}, Ваш оптимальный вес #{ideal_weight} кг."
+  "#{name}, Ваш оптимальный вес #{ideal_weight} кг."
 else
-	"#{name}, Ваш вес уже оптимален."
+  "#{name}, Ваш вес уже оптимален."
 end
