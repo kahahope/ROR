3b3c1586c2a74f68d947430932c6599131a3d86a
kahahope <kahahope@gmail.com>
2016-11-29 17:07:09 +0400

    Create class "Route"

--- Lesson_3/route.rb
+++ Lesson_3/route.rb
@@ -1,1 +1,40 @@
- 
+class Route
+
+  attr_accessor :first_station, :station, :last_station
+
+  def initialize(first_station, last_station)
+    @route = [first_station, last_station]
+    @first_station = first_station
+    @last_station = last_station
+  end
+
+  def list_stations
+    @route
+  end
+
+  def add_station(station)
+    if @route.include?(station)
+      puts "Станция #{station} уже есть в списке"
+    else @route.insert(-2, station)
+    puts "Станция #{station} добавлена в список"
+    end
+  end
+
+  def remove_station(station)
+    if station == @first_station
+      puts "Станция #{station} является начальной, ее удаление невозможно"
+    elsif station == @last_station
+      puts "Станция #{station} является конечной, ее удаление невозможно"
+    elsif @route.delete(station)
+      puts "Станция #{station} удалена из списка"
+    else
+      @route.include?(station)
+      puts "Станции #{station} нет в списке"
+    end
+  end
+
+  def show_list_stations
+    puts "Полный список станций #{@route}"
+  end
+
+end
